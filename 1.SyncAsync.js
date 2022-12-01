8575864618084f373c86f7751efb2dd66845d4af
arfat-beep <arfatrahman08@gmail.com>
2022-12-01 21:54:47 +0600

    callback hell in SyncAsync

--- 1.SyncAsync.js
+++ 1.SyncAsync.js
@@ -8,3 +8,19 @@ console.log("%c I'm excited to teach", "color : red;");
 // this text will be green
 console.log("%cHello", "color: green; background: yellow; font-size: 30px");
 console.log("Hope you are also excited");
+
+// testing asyncronous js
+console.log("Arfatur Rahman");
+
+// handling multiple async which called call back hell
+setTimeout(() => {
+  //   console.log("this is asyncronous");
+  const user = { id: 1 };
+  setTimeout(() => {
+    const products = [{}, {}, {}];
+    setTimeout(() => {
+      const price = [];
+    }, 1000);
+  }, 500);
+}, 1000);
+console.log("another console");
